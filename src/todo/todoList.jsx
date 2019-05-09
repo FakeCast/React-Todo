d979c0f0bc5975f82a7ba9587c30e875cab39eb5
Henrique Feitosa <henrique.feitosa@locaweb.com.br>
2019-05-09 19:38:19 -0300

    First Commit

---
+++ src/todo/todoList.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import IconButton from '../template/iconButton'
+
+export default props => {
+  const renderRows = () => {
+    const list = props.list || []
+    return list.map(todo => (
+      <tr key={todo._id}>
+        <td>{todo.description}</td>
+        <td><IconButton styl='danger' icon='trash-o' onClick={() => props.handleRemove(todo)} /></td>
+      </tr>
+    ))
+  }
+
+  return (
+    <table className='table'>
+      <thead>
+        <tr>
+          <th>Descrição</th>
+        </tr>
+      </thead>
+      <tbody>
+        {renderRows()}
+      </tbody>
+    </table>
+  )
+}
